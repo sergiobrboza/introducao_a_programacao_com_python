ca82336290dc91bc3cd0e2c334002b9aa584cec0
sergiobrboza <sergiobarb7890@gmail.com>
2025-04-26 21:40:06 -0300

    Conclusão

---
+++ capitulo2/exercicio2_5.py
@@ -0,0 +1,8 @@
+# Escreva um programa que calcule a soma de três 
+# variáveis e imprima o resultado.
+x = 2
+y = 3
+z = 4
+soma = x + y + z
+
+print(soma)
---
+++ capitulo2/exercicio2_7.py
@@ -0,0 +1,7 @@
+# Usando as propriedades da divisão e da multiplicação, tente entender como estes resultados são iguais:
+# 0.2 * 6 + 8 * 0.3 + 7 * 0.5 = (20 * + 8 * 30 + 7 * 50 ) / 100
+calculo1 = 0.2 * 6 + 8 * 0.3 + 7 * 0.5
+calculo2 =  (20 * 6 + 8 * 30 + 7 * 50 ) / 100
+
+print(calculo1)
+print(calculo2)
